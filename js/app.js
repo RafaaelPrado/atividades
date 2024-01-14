4716017e58ee6df5f42ac429e6814c816eae6172
RafaaelPrado <152922288+RafaaelPrado@users.noreply.github.com>
2024-01-14 20:58:14 -0300

    Atividade de Lógica de Programação - AluGames

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+function contarJogosAlugados() {
+    console.log()
+}
+
+
+        if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML = "Alugar"
+        alert('O jogo foi devolvido!')
+        } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.innerHTML = "Devolver";
+        }}
+
